6e4a26bd95685458ab9879870f4109f688f1df65
leTienThanh0205 <ltthanh0205@gmail.com>
2024-04-03 05:22:21 +0700

    SetUp_Player

--- Assets/FolderManager/Scripts/Player/PlayerMovement.cs
+++ Assets/FolderManager/Scripts/Player/PlayerMovement.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     BoxCollider2D box;
+    public float walkSpeed = 5f;
+    Vector2 moveInput;
+    public bool IsMoving { get;private set; }
     void Start()
     {
         box = GetComponent<BoxCollider2D>();
@@ -16,5 +21,14 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
     }
 }
