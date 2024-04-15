8d15fe1e8a22e3b554c0805607e3cb8f47c96608
leTienThanh0205 <ltthanh0205@gmail.com>
2024-04-15 23:11:56 +0700

    PlayerMovement_P1

--- Assets/FolderManager/Scripts/Player/PlayerMovement.cs
+++ Assets/FolderManager/Scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,85 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D rb;
-    BoxCollider2D box;
+    
     public float walkSpeed = 5f;
+    public float runSpeed = 5f;
+
     Vector2 moveInput;
-    public bool IsMoving { get;private set; }
+
+    public float CurrentMoveSpeed
+    {
+        get
+        {
+            if (IsMoving)
+            {
+                if(IsRunning)
+                {
+                    return runSpeed;
+                }
+                else
+                {
+                    return walkSpeed;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool IsMoving { get
+        { 
+            return _isMoving;
+        }
+        private set 
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        } 
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+    public bool IsRunning { get
+        { 
+            return _isRunning;
+        }
+        private set 
+        { 
+            _isRunning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
+
+    public bool _isFacingRight = true;
+    public bool IsFacingRight { get
+        {
+            return _isFacingRight;
+        } 
+        private set
+        {
+            if(_isFacingRight != value)
+            {
+                //flip the local sacle to make the player face the opposite direction
+                transform.localScale *= new Vector2(-1, 1);
+            }
+            _isFacingRight = value;
+
+        }
+    }
+
+    Rigidbody2D rb;
+    Animator animator;
+    BoxCollider2D box;
     void Start()
     {
         box = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,11 +95,34 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
     }
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
         IsMoving = moveInput != Vector2.zero;
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            IsFacingRight = true;
+        }else if(moveInput.x < 0 && IsFacingRight)
+        {
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+            IsRunning = true;
+        }else if (context.canceled)
+        {
+            IsRunning = false;
+        }
     }
 }
