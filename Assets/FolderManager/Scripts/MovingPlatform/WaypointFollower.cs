dfe248ae3019716fe7f36bd2fc65bb4a8913775c
leTienThanh0205 <ltthanh0205@gmail.com>
2024-10-15 00:33:59 +0700

    Trap_Saw

--- Assets/FolderManager/Scripts/MovingPlatform/WaypointFollower.cs
+++ Assets/FolderManager/Scripts/MovingPlatform/WaypointFollower.cs
@@ -7,12 +7,10 @@ public class WaypointFollower : MonoBehaviour
     [SerializeField] private GameObject[] waypoint;
     private int currentWaypointIndex = 0;
     [SerializeField] private float speed;
-    private float timer;
-    [SerializeField] private float timeDuration;
 
     void Update()
     {
-        if (Vector2.Distance(waypoint[currentWaypointIndex].transform.position,transform.position) < .1f && timer >= timeDuration)
+        if (Vector2.Distance(waypoint[currentWaypointIndex].transform.position,transform.position) < .1f)
         {
             currentWaypointIndex++;
             if(currentWaypointIndex >= waypoint.Length)
